ec4cab62d3395bb9848e0b2bd0ca77710eca6878
Steve Henderson <hevets@gmail.com>
2016-03-22 11:31:06 -0700

    basic component types

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-class App extends React.Component {
-  render() {
-    return (<div>Hello</div>);
-  }
-}
+// Component #1
+// class App extends React.Component {
+//   render() {
+//     // React.createElement('h1', null, 'Hello World');
+//     return <h1>Hello World</h1>;
+//   }
+// }
+
+
+// Stateless function component
+const App = () => <h1>Hello World</h1>;
 
 export default App;
