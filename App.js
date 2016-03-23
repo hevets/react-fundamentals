4f3cd9699e9cee2f740bf1469f5ee6665a65ced1
Steve Henderson <hevets@gmail.com>
2016-03-23 10:34:14 -0700

    learning about state and how to bind an onChange event

--- App.js
+++ App.js
@@ -2,31 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  render() {
-    // this.props - available within components
-    let txt = this.props.txt;
-    let cat = this.props.cat;
-    return (<h1>{txt} - {cat}</h1>);
+
+  constructor() {
+    super(); // gives us context for this in our Component
+
+    this.state = { txt: 'this is the state text' };
   }
+
+  update(e) {
+    this.setState({ txt: e.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.txt}</h1>
+        <input type="text" onChange={this.update.bind(this)} />
+      </div>
+    );
+  }
+
 }
 
-// Property types ensure we're passing proper types to the properties
-// Can also require certain properties to be provided
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
-};
-
-// Declare default props for the App class
-App.defaultProps = {
-  txt: 'default text',
-  cat: 5
-};
-
-// We can use ReactDOM to render to the page on a given element
 ReactDOM.render(
-  <App txt="steve" cat={5} />,
-    document.getElementById('app')
+  <App />,
+  document.getElementById('app')
 );
 
 export default App;
