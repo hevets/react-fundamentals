79a643b2e24c36c9a4834cf8278b056b44fc6399
Steve Henderson <hevets@gmail.com>
2016-03-23 10:19:06 -0700

    learning about render() method

--- App.js
+++ App.js
@@ -9,7 +9,16 @@ import React from 'react';
 // }
 
 
+// Component #2
 // Stateless function component
-const App = () => <h1>Hello World</h1>;
+// const App = () => <h1>Hello World</h1>;
+
+// Component #3
+// We need to return a single DOM node (which is why we wrap this in a div)
+class App extends React.Component {
+  render() {
+    return (<div><h1>Hello World</h1><b>Bold</b></div>);
+  }
+}
 
 export default App;
