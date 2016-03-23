19d895349f705590150eb49c14fbdcc598ea9854
Steve Henderson <hevets@gmail.com>
2016-03-23 10:28:40 -0700

    learning about props, propTypes and defaultProps

--- App.js
+++ App.js
@@ -1,24 +1,32 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
-// Component #1
-// class App extends React.Component {
-//   render() {
-//     // React.createElement('h1', null, 'Hello World');
-//     return <h1>Hello World</h1>;
-//   }
-// }
-
-
-// Component #2
-// Stateless function component
-// const App = () => <h1>Hello World</h1>;
-
-// Component #3
-// We need to return a single DOM node (which is why we wrap this in a div)
 class App extends React.Component {
   render() {
-    return (<div><h1>Hello World</h1><b>Bold</b></div>);
+    // this.props - available within components
+    let txt = this.props.txt;
+    let cat = this.props.cat;
+    return (<h1>{txt} - {cat}</h1>);
   }
 }
+
+// Property types ensure we're passing proper types to the properties
+// Can also require certain properties to be provided
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired
+};
+
+// Declare default props for the App class
+App.defaultProps = {
+  txt: 'default text',
+  cat: 5
+};
+
+// We can use ReactDOM to render to the page on a given element
+ReactDOM.render(
+  <App txt="steve" cat={5} />,
+    document.getElementById('app')
+);
 
 export default App;
