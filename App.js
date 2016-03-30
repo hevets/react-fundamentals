3e46d4b7994e9ef14fc11ed3546f3ebf6901f1b7
Steve Henderson <hevets@gmail.com>
2016-03-30 10:33:21 -0700

    component will recieve props, shouldComponentUpdate and componentDidUpdate help manage lifecyle of components

--- App.js
+++ App.js
@@ -2,56 +2,39 @@ import React, { PropTypes } from 'react'
 import ReactDOM from 'react-dom'
 
 class App extends React.Component {
-    constructor() {
-      super();
-      this.state = { val: 0 }
-      this.update = this.update.bind(this);
-    }
-
-    componentWillMount() {
-      this.setState({ m: 2 })
-    }
-
-    componentDidMount() {
-      this.inc = setInterval(this.update, 500)
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.inc)
-    }
-
-    update() {
-      this.setState({ val: this.state.val + 1 })
-    }
-
-    render () {
-      console.log('rendering!')
-      return (
-        <button onClick={this.update}>
-          {this.state.val * this.state.m}
-        </button>
-      );
-    }
-}
-
-class Wrapper extends React.Component {
-  mount() {
-    ReactDOM.render(<App />, document.getElementById('a'));
+  constructor() {
+    super()
+    this.update = this.update.bind(this)
+    this.state = { increasing: false }
   }
 
-  unmount() {
-    ReactDOM.unmountComponentAtNode(document.getElementById('a'));
+  update() {
+    ReactDOM.render(<App val={this.props.val+1} />, document.getElementById('app'))
   }
 
-  render() {
+  componentWillReceiveProps(nextProps) {
+    this.setState({increasing: nextProps.val > this.props.val })
+  }
+
+  // Good time to optimize component
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.val % 5 === 0
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('prevProps', prevProps)
+  }
+
+  render () {
+    console.log(this.state.increasing);
     return (
-      <div>
-        <button onClick={this.mount}>Mount</button>
-        <button onClick={this.unmount}>Unmount</button>
-        <div id="a"></div>
-      </div>
+      <button onClick={this.update}>
+        {this.props.val}
+      </button>
     )
   }
 }
 
-export default Wrapper;
+App.defaultProps = { val: 0 }
+
+export default App;
