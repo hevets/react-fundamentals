31d6037562ca55fdf46502a8e2c9e249bc039e9b
Steve Henderson <hevets@gmail.com>
2016-03-30 10:19:14 -0700

    Adds ability to listen to component events, mount and unmount components from the dom

--- App.js
+++ App.js
@@ -1,17 +1,54 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
+import ReactDOM from 'react-dom'
 
 class App extends React.Component {
-  render () {
-    return <Button>I <Heart /> React</Button>
-  }
+
+    constructor() {
+      super();
+      this.state = { val: 0 }
+      this.update = this.update.bind(this);
+    }
+
+    componentWillMount() {
+      console.log('mounting')
+    }
+
+    componentDidMount() {
+      console.log('mounted');
+    }
+
+    componentWillUnmount() {
+        console.log('bye!')
+    }
+
+    update() {
+      this.setState({ val: this.state.val + 1 })
+    }
+
+    render () {
+      console.log('rendering!')
+      return <button onClick={this.update}>{this.state.val}</button>
+    }
 }
 
-class Button extends React.Component {
+class Wrapper extends React.Component {
+  mount() {
+    ReactDOM.render(<App />, document.getElementById('a'));
+  }
+
+  unmount() {
+    ReactDOM.unmountComponentAtNode(document.getElementById('a'));
+  }
+
   render() {
-    return <button>{this.props.children}</button>
+    return (
+      <div>
+        <button onClick={this.mount}>Mount</button>
+        <button onClick={this.unmount}>Unmount</button>
+        <div id="a"></div>
+      </div>
+    )
   }
 }
 
-const Heart = () => <span className="glyphicon glyphicon-heart"></span>
-
-export default App
+export default Wrapper;
